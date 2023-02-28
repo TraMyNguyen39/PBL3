fe5c64cae807b9b96ebb6d6d9ab3a9a331b699a3
mymyloveyoujung <tram8319@gmail.com>
2023-02-28 20:43:36 +0700

    header-user

---
+++ Pages/User/HomePage.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CheeseBurger.Pages
+{
+    public class HomePageModel : PageModel
+    {
+        private readonly ILogger<HomePageModel> _logger;
+
+        public HomePageModel(ILogger<HomePageModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
